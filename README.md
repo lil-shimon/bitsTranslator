6ab8e9b17dd0e27c2e25e999e9be7e680ce9bf83
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-11-12 11:49:22 +0900

    docs(readme): added readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# bit-translator
+
+## 仕様
+8bitを2bitずつの4グループにわけて判定
+
+- 11の場合 -> PERFECT
+- 10の場合 -> YES
+- 01の場合 -> NO
+- 00の場合 -> BAD
+
+## 例
+11100100の場合  
+PERFECT | YES | NO | BAD
--- translate.js
+++ translate.js
@@ -4,8 +4,8 @@ const zeroSuppress = (num, len) => num.toString(2).slice(len);
 for (let i = 0; i < 4; i ++) {
 
     let PERFECT = !!(num >> (i * 2) & 1 && num >> (i * 2 + 1) & 1);
-    let YES = !!(num >> (i * 2) & 1);
-    let NO = !!(num >> (i * 2 + 1) & 1);
+    let YES = !!(num >> (i * 2 + 1) & 1);
+    let NO = !!(num >> (i * 2) & 1);
 
     let res = PERFECT ? "PERFECT" : YES ? "YES" : NO ? "NO" : "BAD";
 
